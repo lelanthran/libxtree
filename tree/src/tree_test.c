61a8f6117e0b53ddc0f349f0d7bef465b5f56dfa
lelanthran <lelanthran@gmail.com>
2026-08-14 12:32:40 +0200

    Initial commit; need to rename this repo.

---
+++ tree/src/tree_test.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("testing tree library\n");
+  return 0;
+}
+
---
+++ tree/src/xtree.h
@@ -0,0 +1,17 @@
+
+#ifndef H_XTREE
+#define H_XTREE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif
+
