35e4a7113dbafcf08679e3d0ef460c8cd068b928
lelanthran <lelanthran@gmail.com>
2026-09-14 15:03:55 +0200

    Starts the emitter design.

---
+++ xtree/src/xtree_emit.c
@@ -0,0 +1,5 @@
+
+
+#include "xtree_emit.h"
+
+
---
+++ xtree/src/xtree_emit.h
@@ -0,0 +1,18 @@
+
+#ifndef H_XTREE_EMIT
+#define H_XTREE_EMIT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif
+
+
